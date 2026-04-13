2339b1739217d88b50b6bc6d5ad73b8b9d3496ba
MriteshAdak <mriteshadak@outlook.com>
2026-04-13 10:16:02 +0200

    feat: implement carousel component to display project cards

--- src/app/components/carousel/carousel.ts
+++ src/app/components/carousel/carousel.ts
@@ -1,9 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Card } from '../card/card';
+import { Project } from '../../interfaces/project';
 
 @Component({
   selector: 'app-carousel',
-  imports: [],
-  template: ` <p>carousel works!</p> `,
+  imports: [Card],
+  template: ` <div>
+    @for (project of projects(); track project.id) {
+      <app-card [project]="project"></app-card>
+    }
+  </div> `,
   styles: ``,
 })
-export class Carousel {}
+export class Carousel {
+  projects = input.required<Project[]>();
+}
